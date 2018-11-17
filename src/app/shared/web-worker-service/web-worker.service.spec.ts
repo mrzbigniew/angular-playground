f1f63f5fb1a791e8e067bf9dfb018abdafe613ab
Zbigniew Jędraczka <mr.zbigniew@gmail.com>
2018-11-17 02:28:57 +0100

    code fix

--- src/app/shared/web-worker-service/web-worker.service.spec.ts
+++ src/app/shared/web-worker-service/web-worker.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MyWebWorkerService } from './web-worker.service';
+import { WebWorkerService } from './web-worker.service';
 
 describe('WebWorkerService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: MyWebWorkerService = TestBed.get(MyWebWorkerService);
+    const service: WebWorkerService = TestBed.get(WebWorkerService);
     expect(service).toBeTruthy();
   });
 });
